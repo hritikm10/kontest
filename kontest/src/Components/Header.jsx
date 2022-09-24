9455bd7e41723b29ba210f231a8c8d209590d6fe
Hritik Middha <hritikmiddha10@gmail.com>
2022-09-24 22:07:42 +0530

    updated cards

--- kontest/src/Components/Header.jsx
+++ kontest/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div style={{textAlign : "center", marginBottom : "2rem"}}>Navbar</div>
   )
 }
 
--- kontest/src/Components/TotalData.jsx
+++ kontest/src/Components/TotalData.jsx
@@ -18,21 +18,25 @@ useEffect(() => {
   return (
     <>
     {
-        Data.map((contest)=>
-        {
+        Data.map((contest,key)=>
+        {   
             return (
                 <>
                    <center>
-                   <div className="card">
+                   <div className="card" key={key}>
                     <div className="innercard">
                         <div className="title">Name : {contest.name}</div>
                         <div className="date">Date : {contest.start_time.substring(0, 10)}</div>
-                        <a href={contest.url} target="_blank">Go To Contest</a>
                         <div className="stime">Start time: {(parseInt(contest.start_time.substring(11, 13)) <= 12 ?  contest.start_time.substring(11, 13) :  parseInt(contest.start_time.substring(11, 13)) - 12) + contest.start_time.substring(13, 19) + (parseInt(contest.start_time.substring(11, 13)) <= 12 ? " AM"  : " PM")}</div>
                         <div className="duration">Duration of test :{Math.floor(contest.duration / 86400) == "0" ? contest.duration/3600 + " Hours " : Math.floor(contest.duration / 86400) + " Days"}</div>
                         <div className="site">Platform : {contest.site}</div>
                         <div className="status">Status : {contest.status == "CODING" ? "Ongoing" : "Upcoming"}</div>
                         <div className="in24">In 24 hours : {contest.in_24_hours}</div>
+                        <div className="cardButtons">
+                            <a className='cardButton' href={contest.url} target="_blank"><span>Go To Contest</span></a>
+                            <a className='cardButton' href="#" target="_blank"><span>Add to calender</span></a>
+                        </div>
+                       
                     </div>
                    </div>
                    </center>
