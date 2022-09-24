38fe08a98d6b597620148ab6a666e49e1ab28d2e
Hritik Middha <hritikmiddha10@gmail.com>
2022-09-24 17:17:46 +0530

    Update TotalData.jsx

--- kontest/src/Components/TotalData.jsx
+++ kontest/src/Components/TotalData.jsx
@@ -25,14 +25,14 @@ useEffect(() => {
                    <center>
                    <div className="card">
                     <div className="innercard">
-                        <div className="title">{contest.name}</div>
-                        <div className="date">{contest.start_time.substring(0, 10)}</div>
+                        <div className="title">Name : {contest.name}</div>
+                        <div className="date">Date : {contest.start_time.substring(0, 10)}</div>
                         <a href={contest.url} target="_blank">Go To Contest</a>
-                        <div className="stime">{(parseInt(contest.start_time.substring(11, 13)) <= 12 ?  contest.start_time.substring(11, 13) :  parseInt(contest.start_time.substring(11, 13)) - 12) + contest.start_time.substring(13, 19) + (parseInt(contest.start_time.substring(11, 13)) <= 12 ? " AM"  : " PM")}</div>
-                        <div className="duration">{Math.floor(contest.duration / 86400) == "0" ? contest.duration/3600 + " Hours " : Math.floor(contest.duration / 86400) + " Days"}</div>
-                        <div className="site">{contest.site}</div>
-                        <div className="status">{contest.status == "CODING" ? "Ongoing" : "Upcoming"}</div>
-                        <div className="in24">{contest.in_24_hours}</div>
+                        <div className="stime">Start time: {(parseInt(contest.start_time.substring(11, 13)) <= 12 ?  contest.start_time.substring(11, 13) :  parseInt(contest.start_time.substring(11, 13)) - 12) + contest.start_time.substring(13, 19) + (parseInt(contest.start_time.substring(11, 13)) <= 12 ? " AM"  : " PM")}</div>
+                        <div className="duration">Duration of test :{Math.floor(contest.duration / 86400) == "0" ? contest.duration/3600 + " Hours " : Math.floor(contest.duration / 86400) + " Days"}</div>
+                        <div className="site">Platform : {contest.site}</div>
+                        <div className="status">Status : {contest.status == "CODING" ? "Ongoing" : "Upcoming"}</div>
+                        <div className="in24">In 24 hours : {contest.in_24_hours}</div>
                     </div>
                    </div>
                    </center>
